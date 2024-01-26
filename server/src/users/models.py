69ead2b8cfa37c9abc2b3bb62170d50f00385c1e
Jesse Watson <jesse.watson@ondefend.com>
2024-01-26 16:54:40 -0500

    integrated alembic for db migrations

--- server/src/users/models.py
+++ server/src/users/models.py
@@ -1,12 +1,12 @@
-from database import Base
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, false
+from src.database import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, server_default=false())
